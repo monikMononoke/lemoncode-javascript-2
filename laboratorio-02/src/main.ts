b0c2cb42e47129991777190a66b6febc42c936bb
monikMononoke <monik.stefanova@gmail.com>
2024-01-31 14:31:35 +0100

    laboratorio 02

---
+++ laboratorio-02/src/main.ts
@@ -0,0 +1,6 @@
+import "./style.css";
+
+console.log("Cena de cumpleaños ");
+console.log("El ticket total menos la bebida " + (120-18) + " €");
+console.log("Cuanto tiene que pagar cada comensal " + ((120-18)/6) + " €");
+
