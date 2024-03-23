61f57c3ad2212766eab554d937c713c6fa6188eb
monikMononoke <monik.stefanova@gmail.com>
2024-03-23 10:25:30 +0100

    laboratorio-08.3-apartado1

---
+++ laboratorio-08.3(apartado-1)/src/main.ts
@@ -0,0 +1,13 @@
+import { animales } from "./datos";
+import { barajarAnimales } from "./motor";
+import "./style.css";
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log(barajarAnimales(animales));
+    console.log(barajarAnimales(animales));
+    console.log(barajarAnimales(animales));
+    console.log(barajarAnimales(animales));
+    console.log(barajarAnimales(animales));
+    console.log(barajarAnimales(animales));
+})
