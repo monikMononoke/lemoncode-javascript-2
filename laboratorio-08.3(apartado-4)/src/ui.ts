6d797c1d7c5cf80b514e98e2639a20c0c7a60f46
monikMononoke <monik.stefanova@gmail.com>
2024-03-26 08:46:56 +0100

    laboratorio-08.3-apartado4

--- laboratorio-08.3(apartado-4)/src/ui.ts
+++ laboratorio-08.3(apartado-4)/src/ui.ts
@@ -1,42 +1,15 @@
-const crearDiv = (nombreClase: string): HTMLDivElement => {
-    const div = document.createElement('div');
-    div.classList.add(nombreClase);
-    return div;
-}
-
-const mostrarImagen = (contenedor: HTMLDivElement, numImagen: number) => {
-    contenedor.innerHTML = `
-    <img src="./imagenes/${numImagen}.png" alt="animal-${numImagen}"/>
-    `
-    contenedor.className = "carta-boca-arriba";
-
-    setTimeout(() => {
-        contenedor.className = "carta-boca-abajo";
-        contenedor.innerHTML = "";
-    }, 3000)
-};
-
 export const pintarJuego = () => {
-    const divPrincipal = document.getElementById("contenedor");
+    const cardList: NodeListOf<HTMLDivElement> = document.querySelectorAll('.carta');
 
-    if (divPrincipal && divPrincipal instanceof HTMLDivElement) {
-        const contenedorCartas = crearDiv("contenedor-cartas");
-        divPrincipal.appendChild(contenedorCartas);
+    const imagenes: NodeListOf<HTMLImageElement> = document.querySelectorAll('.imagenes');
 
-        const divCarta1 = crearDiv("carta-boca-abajo");
-        contenedorCartas.appendChild(divCarta1);
-
-        divCarta1.addEventListener("click", () => {
-            mostrarImagen(divCarta1, 1);
-        })
-
-        const divCarta2 = crearDiv("carta-boca-abajo");
-        contenedorCartas.appendChild(divCarta2);
-
-        divCarta2.addEventListener("click", () => {
-            mostrarImagen(divCarta2, 2);
-        })
-    }
+    cardList.forEach((card, index) => {
+        card.addEventListener("click", () => {
+            card.style.transform = "rotateY(-180deg)";
+            imagenes[index].src = `./imagenes/${index + 1}.png`;
+            console.log(imagenes);
+        });
+    });
 }
 
 
