61b41efd41eb04886cdc80d18dd8e3a6c4c7b942
Monika Stefanova <monik.stefanova@gmail.com>
2024-04-07 21:07:54 +0200

    laboratorio-08.3-implementacion

--- laboratorio-08.3(prueba-implementacion)/src/constantes.ts
+++
@@ -1,3 +0,0 @@
-export const divsCartas: NodeListOf<HTMLDivElement> = document.querySelectorAll('.carta');
-export const imagenes: NodeListOf<HTMLImageElement> = document.querySelectorAll('.imagenes');
-
--- laboratorio-08.3(prueba-implementacion)/src/main.ts
+++
@@ -1,5 +0,0 @@
-import { cartas } from "./modelo";
-import { crearTablero } from "./ui";
-
-document.addEventListener("DOMContentLoaded", crearTablero);
-console.log(cartas);
